739e455e9fa1e8f50803e2307502249559d3a300
Jeremy Pek <jeremy.pek@xendit.co>
2023-05-18 11:04:14 +0800

    fix: changes to readme, remove all xencrypt references

--- tests/secure-session.test.ts
+++ tests/secure-session.test.ts
@@ -2,12 +2,12 @@ import forge from 'node-forge';
 import { XenIssuing } from '../src';
 import { SecureSession } from '../src/lib/secure-session';
 
-describe('XenCrypt', () => {
-  let xenCrypt: SecureSession;
+describe('SecureSession', () => {
+  let secureSession: SecureSession;
   beforeAll(() => {
     const keys = forge.pki.rsa.generateKeyPair(2048);
     const pubKey = forge.pki.publicKeyToPem(keys.publicKey);
-    xenCrypt = XenIssuing.createSecureSession(pubKey);
+    secureSession = XenIssuing.createSecureSession(pubKey);
   });
   it("'generateSessionKey' should generate Session Key in base64 format", () => {
     // Arrange
@@ -25,7 +25,7 @@ describe('XenCrypt', () => {
     const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
 
     // Act
-    const sessionId = xenCrypt.encryptKey();
+    const sessionId = secureSession.encryptKey();
 
     // Assert
     expect(base64regex.test(sessionId)).toBe(true);
@@ -34,12 +34,12 @@ describe('XenCrypt', () => {
   it('should decrypt plain text', () => {
     // Arrange
     const plain = 'test';
-    const iv = xenCrypt.generateIV();
+    const iv = secureSession.generateIV();
     const ivB64 = forge.util.encode64(iv);
-    const encryptedPlain = xenCrypt.encrypt(plain, iv);
+    const encryptedPlain = secureSession.encrypt(plain, iv);
 
     // Act
-    const decryptedPlain = xenCrypt.decryptCardData(ivB64, encryptedPlain);
+    const decryptedPlain = secureSession.decryptCardData(ivB64, encryptedPlain);
 
     // Assert
     expect(decryptedPlain).toEqual(
@@ -50,13 +50,13 @@ describe('XenCrypt', () => {
   it('should not decrypt plain text if provided different iv then was provided during encryption', () => {
     // Arrange
     const plain = 'test';
-    const iv = xenCrypt.generateIV();
+    const iv = secureSession.generateIV();
     const secondIv = forge.random.getBytesSync(16);
     const ivB64 = forge.util.encode64(secondIv);
-    const encryptedPlain = xenCrypt.encrypt(plain, iv);
+    const encryptedPlain = secureSession.encrypt(plain, iv);
 
     // Act
-    const decryptedPlain = xenCrypt.decryptCardData(
+    const decryptedPlain = secureSession.decryptCardData(
       ivB64,
       encryptedPlain,
     );
@@ -70,11 +70,11 @@ describe('XenCrypt', () => {
   it('should not decrypt plain text if the provided during decryption iv is not encoded', () => {
     // Arrange
     const plain = 'test';
-    const iv = xenCrypt.generateIV();
-    const encryptedPlain = xenCrypt.encrypt(plain, iv);
+    const iv = secureSession.generateIV();
+    const encryptedPlain = secureSession.encrypt(plain, iv);
 
     // Act
-    const decryptedPlain = xenCrypt.decryptCardData(iv, encryptedPlain);
+    const decryptedPlain = secureSession.decryptCardData(iv, encryptedPlain);
 
     // Assert
     expect(decryptedPlain).not.toEqual(
--- tests/test.ts
+++ tests/test.ts
@@ -1,4 +1,3 @@
-import { SecureSession } from '../src/lib/secure-session';
 import { XenIssuing } from '../src';
 
 const pubkey = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtnraE8cT6G4CGhwVIan7EqmcMue7cFgtgTGzbiZS4buNuDiPN18iiOGSIdXGAwte0CS8o6qjwKKigHtg2XMBxThZctxj0T5lKa2JatqH2SnVV8c2GdoLJEqpmr73P7/tkCWGCds0KZriupPKgU+JSqGWhCKkKectUgkz8F/GdQIHUHxRdKQ4rFLbuHCwzncL3ZolUruiFIEZmz435B2iKdBXlxOx9MizxsH8IDiFgSRgId+xp0V2eStz8dKaN+N5C+QnDtVmXJWlkks0WygMxqXPQVaaiLNwmwvCSV+9zY3NU+Xz4+EfwZ4+Mvc7SxUq7IVwwmT+g2Vwq9xEWedMmwIDAQAB';
@@ -6,11 +5,15 @@ const pkstr = `-----BEGIN PUBLIC KEY-----${pubkey}-----END PUBLIC KEY-----`;
 
 // const sessionKey = SecureSession.generateSessionKey();
 // console.log('sessionKey', sessionKey);
-const secureSession = XenIssuing.createSecureSession(pkstr); // add sessionKey as parameter to test
 
-const sessionId = secureSession.getKey();
+// add sessionKey as parameter to test
+// eg. XenIssuing.createSecureSession(pkstr, sessionKey);
+const secureSession = XenIssuing.createSecureSession(pkstr);
 
+const sessionId = secureSession.getKey(); // to be used for API calls(eg. get card pan,cvv,etc)
 console.log('sessionId', sessionId);
+
+// Call API (eg. get card pan,cvv,etc) to get secret and iv from response
 const secret = '';
 const iv = '';
 console.log('decrypt', secureSession.decryptCardData(iv, secret));
